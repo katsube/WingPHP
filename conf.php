4c673c762fcb586628aa6d0cc85f4ca527289173
katsubemakito@gmail.com <katsubemakito@gmail.com@b1f6b2e8-872c-11de-bed6-6d316fbe0868>
2013-09-02 06:30:52 +0000

    conf.php からConf['cache']['api_*']系を削除

--- conf.php
+++ conf.php
@@ -81,11 +81,6 @@ $Conf = array(
 		, 'db_use'    => false					//ModelのDB系メソッドでキャッシュするか
 		, 'db_pre'    => '__lib.dbcache'		//ModelのDB系メソッドのキャッシュIDの先頭文字列
 		, 'db_expire' => 3600					//キャッシュ寿命
-		
-		//WebAPI系
-		, 'api_use'    => false					//WebAPI系ライブラリでキャッシュするか
-		, 'api_pre'    => '__lib.netapicache'	//WebAPI系ライブラリのキャッシュIDの先頭文字列
-		, 'api_expire' => 3600					//キャッシュ寿命
 	)
 	
 	//■View設定
