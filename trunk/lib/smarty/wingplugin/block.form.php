441ce40aefef139f862bc14e0f37037ad1f6bbc4
katsubemakito@gmail.com <katsubemakito@gmail.com@b1f6b2e8-872c-11de-bed6-6d316fbe0868>
2013-09-09 15:58:37 +0000

    Smarty用プラグイン form を追加

---
+++ trunk/lib/smarty/wingplugin/block.form.php
@@ -0,0 +1,25 @@
+﻿<?php
+
+function smarty_block_form($params, $content, &$smarty, &$repeat){
+	if(!$repeat){
+		$attrs = array('action', 'method', 'name', 'id', 'class', 'target', 'accept', 'accept-charset', 'enctype');
+		$attr  = array();
+
+		foreach($attrs as $name){
+			if(array_key_exists($name, $params)){
+					$attr[$name] = $params[$name];
+			}
+		}
+
+		$tag = '<form';
+		foreach($attr as $name => $value){
+			$name   = htmlspecialchars($name);
+			$value  = htmlspecialchars($value);
+
+			$tag   .= sprintf(' %s="%s"', $name, $value);
+		}
+		$tag .= '>';
+
+		return($tag . $content . '</form>');
+	}
+}
