ace5c8e362ed0306cabd84d65b9aeb128042e322
katsubemakito@gmail.com <katsubemakito@gmail.com@b1f6b2e8-872c-11de-bed6-6d316fbe0868>
2015-03-02 10:20:37 +0000

    Smartyのプラグイン「request」追加。

---
+++ trunk/lib/Smarty/wingplugin/function.request.php
@@ -0,0 +1,33 @@
+﻿<?php
+/** 
+ * $_REQUEST変数の指定キーの内容を取得（エスケープ付き）
+ *
+ */
+function smarty_function_request($params, &$smarty){
+	// クエリ名の指定があるか
+	if( array_key_exists('key', $params) ){
+		//REQUEST内に存在するか
+		if( array_key_exists($params['key'], $_REQUEST) ){
+			$value = $_REQUEST[$params['key']];
+			
+			//エスケープし返却
+			if( array_key_exists('escape', $params) ){
+				switch($params['escape']){
+					case 'url':
+						return(urlencode($value));
+						break;
+					
+					case 'html':
+					default:
+						return(htmlspecialchars($value));
+				}
+			}
+			//そのまま返却
+			else{
+				return($value);
+			}
+		}
+	}
+
+	return(null);
+}
