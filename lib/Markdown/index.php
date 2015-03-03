c648aa5e291a507709a9a50e05f156a44b359bca
katsubemakito@gmail.com <katsubemakito@gmail.com@b1f6b2e8-872c-11de-bed6-6d316fbe0868>
2015-03-03 07:21:01 +0000

    Markdown/index.php 追加

---
+++ lib/Markdown/index.php
@@ -0,0 +1,66 @@
+<?php
+/* [WingPHP]
+ *  - lib/Markdown/index.php
+ *  
+ * The MIT License
+ * Copyright (c) 2009 WingPHP < http://wingphp.net >
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+ 
+/**
+ * Markdownクラス
+ * 
+ * example.<code>
+ *     uselib('Markdown');
+ *
+ *     $md = new Markdown();
+ *     echo $md->get("'Hello _MarkeDown_!'");
+ * </code>
+ *
+ * @package    Markdown
+ * @copyright  2015 WingPHP
+ * @author     M.Katsube < katsubemakito@gmail.com >
+ * @license    The MIT License
+ * @access     public
+ */
+class Markdown {
+	private $pd = null;
+
+	/**
+	 * コンストラクタ
+	 *
+	 * @access public
+	 */
+	public function __construct(){
+		uselib('Markdown/parsedown/Parsedown');
+		$this->pd = new Parsedown();
+	}
+
+	/**
+	 * TextをHTMLに変換
+	 *
+	 * @param  $text  string  Markdown記法で書かれた文字列
+	 * @access public
+	 */
+	function get($text){
+		return(
+			$this->pd->text($text)
+		);
+	}
+}
