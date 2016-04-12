a28aca045adc08ee47bdc91d2d0aa59d2c1e63d8
Makito Katsube <katsubemakito@gmail.com>
2016-04-12 16:42:23 +0000

    make skeleton for php.

--- bin/php/wing.php
+++ bin/php/wing.php
@@ -1,2 +1,63 @@
 <?php
-echo "I am wing.php\n";
+
+
+init($argv[1], $argv[2]);
+
+
+/**
+ * Initialize
+ */
+function init($shelldir, $mode){
+    //---------------------------------
+    // change current diretory
+    //---------------------------------
+    if( is_dir($shelldir) && chdir($shelldir) ){
+        ;
+    }
+    else{
+        error"Can not change current directory ($shelldir)");
+        exit;
+    }
+
+    //---------------------------------
+    // Do it
+    //---------------------------------
+    switch( $mode ){
+        case 'SQL':
+            run_sql();
+            break;
+        
+        case 'CREATE':
+            run_create();
+            break;
+        
+        default:
+            error("undefine mode ($mode)");
+            exit;
+    }
+
+}
+
+
+/**
+ * Run SQL
+ */
+function run_sql(){
+    
+}
+
+/**
+ * Create Template
+ */
+function run_create(){
+    
+}
+
+
+/**
+ * Error
+ */
+function error($msg){
+    echo "*** Error\n";
+    echo "$msg\n";
+}
