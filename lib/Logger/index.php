86c4b4fc0bd8ca45324cff74538d115b4f431add
Makito Katsube <katsubemakito@gmail.com>
2016-05-22 09:04:02 +0000

    make lib/Logger #4

---
+++ lib/Logger/index.php
@@ -0,0 +1,58 @@
+<?php
+/* [WingPHP]
+ *  - lib/Logger/index.php
+ *  
+ * The MIT License
+ * Copyright (c) 2016 WingPHP < http://wingphp.net >
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+ 
+/**
+ * Loggerクラス
+ * 
+ * example.<code>
+ *     uselib('Logger');
+ *
+ *     $log = new Logger();
+ *
+ *     // write log
+ *     $log->debug("debug message");
+ *     $log->info("information message");
+ *     $log->warning("warning message");
+ *     $log->error("error message");
+ * </code>
+ *
+ * @package    Logger
+ * @copyright  2016 WingPHP
+ * @author     M.Katsube < katsubemakito@gmail.com >
+ * @license    The MIT License
+ * @access     public
+ */
+class Logger {
+
+	/**
+	 * コンストラクタ
+	 *
+	 * @access public
+	 */
+	public function __construct(){
+	    ;
+	}
+
+}
