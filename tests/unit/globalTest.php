128c8499e13ed963f468347b7cfd01f44c3c6510
Makito Katsube <katsubemakito@gmail.com>
2016-05-25 08:31:26 +0000

    add Test array_end()

--- tests/unit/globalTest.php
+++ tests/unit/globalTest.php
@@ -6,7 +6,7 @@ class globalTest extends \Codeception\TestCase\Test
     use \Codeception\Specify;
 
     const GENUNIQID_LEN  = 40;      //gen_uniqid() 生成される文字列長
-    const GENUNIQID_LOOP = 100;     //gen_uniqid() ユニーク性を検証する個数
+    const GENUNIQID_LOOP = 10;      //gen_uniqid() ユニーク性を検証する個数
 
     /**
      * @var \UnitTester
@@ -19,6 +19,23 @@ class globalTest extends \Codeception\TestCase\Test
     }
 
     protected function _after(){
+    }
+    
+
+    public function test_array_end(){
+        $this->specify("value is last value", function() {
+            $arr = array("apple", "banana", "muscat");
+
+            $this->assertTrue(array_end($arr) === "muscat");
+        });
+
+        $this->specify("not delete", function() {
+            $arr = array("apple", "banana", "muscat");
+            $tmp = array_end($arr);
+
+            $this->assertTrue(count($arr) === 3);
+            $this->assertTrue($arr === array("apple", "banana", "muscat"));
+        });
     }
 
     public function test_gen_uniqid(){
