e9822642e4d5957a870b8b80c38b8c27c28f8454
Makito Katsube <katsubemakito@gmail.com>
2016-05-27 15:05:17 +0000

    add test addlogfile()

--- tests/unit/globalFuncton.UnitTest.php
+++ tests/unit/globalFuncton.UnitTest.php
@@ -1,6 +1,6 @@
 <?php
-require_once('conf.php');
-require_once('lib/global.php');
+require_once('../conf.php');
+require_once('../lib/global.php');
 
 class globalFunctionUnitTest extends PHPUnit_Framework_TestCase
 {
@@ -34,28 +34,142 @@ class globalFunctionUnitTest extends PHPUnit_Framework_TestCase
     }
 
     /**
-     * test addlogfile()
+     * test addlogfile() - 1
      */
-    public function testFunction_addlogfile(){
+    public function testFunction_addlogfile1(){
         global $Conf;
         $time = time();
-        
-        //$Confにない識別子を与えるとファイルが生成されずfalseが返されるか
+
+        //$Confにない識別子を与えると(もしくは何も与えないと)falseが返されるか
+        $this->assertFalse( addlogfile() );
         $this->assertFalse( addlogfile('%%__FOOBAR__%%', $time) );
         
         //ログファイルが正しく生成されるか
+        $filename = $this->_addlogfile_makefilename($time);
+        $this->assertTrue( addlogfile('TEST', $str1) );
+        $this->assertFileExists($filename);
+
+        unlink($filename);
+    }
+
+    /**
+     * test addlogfile() - 2
+     */
+    public function testFunction_addlogfile2(){
+        global $Conf;
+        $time     = time();
+        $filename = $this->_addlogfile_makefilename($time);
+        $str1     = 'HelloWorld';
         
         //ログファイルに記録されるか
-        
-        //ログファイルに記録されるか (可変長引数)
+        addlogfile('TEST', $str1);
+        $buff = explode($Conf['Log']['separate'], rtrim(file_get_contents($filename)) );
+        $this->assertEquals($buff[2], $str1);
+        unlink($filename);
+    }
+    
+    /**
+     * test addlogfile() - 3
+     */
+    public function testFunction_addlogfile3(){
+        global $Conf;
+        $time     = time();
+        $filename = $this->_addlogfile_makefilename($time);
+        $str1     = 'HelloWorld';
+        $str2     = 'fizzbuzz';
+        $str3     = 'foobar';
 
+        //ログファイルに記録されるか (可変長引数)
+        addlogfile('TEST', $str1, $str2, $str3);
+        $buff = explode($Conf['Log']['separate'], rtrim(file_get_contents($filename)) );
+        $this->assertEquals($buff[2], $str1);
+        $this->assertEquals($buff[3], $str2);
+        $this->assertEquals($buff[4], $str3);
+        unlink($filename);
+    }
+    
+    /**
+     * test addlogfile() - 4
+     */
+    public function testFunction_addlogfile4(){
+        global $Conf;
+        $time     = time();
+        $filename = $this->_addlogfile_makefilename($time);
+        $str1     = 'HelloWorld';
+        $str2     = 'fizzbuzz';
+        $str3     = 'foobar';
+        $arr      = array('apple', 'banana', 'muscat');
+    
         //ログファイルに記録されるか (配列が展開されるか)
-        
+        addlogfile('TEST', $str1, $arr, $str2, $str3);
+        $buff = explode($Conf['Log']['separate'], rtrim(file_get_contents($filename)) );
+        $this->assertEquals($buff[2], $str1);
+        $this->assertEquals($buff[3], $arr[0]);
+        $this->assertEquals($buff[4], $arr[1]);
+        $this->assertEquals($buff[5], $arr[2]);
+        $this->assertEquals($buff[6], $str2);
+        $this->assertEquals($buff[7], $str3);
+        unlink($filename);
+    }
+
+    /**
+     * test addlogfile() - 5
+     */
+    public function testFunction_addlogfile5(){
+        global $Conf;
+        $time     = time();
+        $filename = $this->_addlogfile_makefilename($time);
+        $str1     = sprintf("Hello%sWorld", $Conf['Log']['separate']);
+        $str2     = "fizz\nbuzz";
+        $str3     = "foo\rbar";
+
         //ログファイルに記録される際に特定文字列が削除されるか
+        addlogfile('TEST', $str1, $str2, $str3);
+        $buff = explode($Conf['Log']['separate'], rtrim(file_get_contents($filename)) );
+        $this->assertEquals($buff[2], str_replace($Conf['Log']['separate'], '', $str1) );
+        $this->assertEquals($buff[3], str_replace("\n", '', $str2) );
+        $this->assertEquals($buff[4], str_replace("\r", '', $str3) );
+        unlink($filename);
+    }
+
+    /**
+     * test addlogfile() - 6
+     */
+    public function testFunction_addlogfile6(){
+        global $Conf;
+        $time     = time();
+        $filename = $this->_addlogfile_makefilename($time);
+        $str1     = 'HelloWorld';
+        $str2     = 'fizzbuzz';
+        $str3     = 'foobar';
 
         //ログファイルに追記されるか
+        addlogfile('TEST', $str1, $str2);
+        addlogfile('TEST', $str3);
+        addlogfile('TEST', $str2, $str1);
+        
+        $tmp   = file($filename);
+        $buff1 = explode($Conf['Log']['separate'], rtrim($tmp[0]) );
+        $buff2 = explode($Conf['Log']['separate'], rtrim($tmp[1]) );
+        $buff3 = explode($Conf['Log']['separate'], rtrim($tmp[2]) );
 
+        $this->assertEquals($buff1[2], $str1);
+        $this->assertEquals($buff1[3], $str2);
+        $this->assertEquals($buff2[2], $str3);
+        $this->assertEquals($buff3[2], $str2);
+        $this->assertEquals($buff3[3], $str1);
+        unlink($filename);
     }
+
+    private function _addlogfile_makefilename($time){
+        global $Conf;
+        return(
+            sprintf('%s/%s%s.%s', $Conf['Log']['dir'], $Conf['Log']['file']['TEST'], date($Conf['Log']['add'], $time), $Conf['Log']['ext'])
+        );
+    }
+    
+    
+    
 
     /**
      * test lockfwrite()
