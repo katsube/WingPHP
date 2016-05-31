834f3aea3ed3354767be7d0f264135525525cc09
Makito Katsube <katsubemakito@gmail.com>
2016-05-31 11:22:00 +0000

    add PrivateTest Class

--- tests/define.php
+++ tests/define.php
@@ -15,3 +15,26 @@ class TestsUtil{
         );
     }
 }
+
+
+// Thanx! http://qiita.com/kumazo@github/items/45d956b0e66cd0b5e0bd
+//   使用例
+//     $target = TestPrivate::on( new Target() );
+//     $actual = $target->anyPrivateMethod();
+class TestPrivate {
+    private $target;
+
+    private function __construct($target) {
+        $this->target = $target;
+    }
+
+    public static function on($target) {
+        return( new self($target) );
+    }
+
+    public function __call($name, $args) {
+        $method = new  ReflectionMethod($this->target, $name);
+        $method->setAccessible(true);
+        return( $method->invokeArgs($this->target, $args) );
+    }
+}
