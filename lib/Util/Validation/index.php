f0bbcf18c12d3c72670e26ab54e06314279848d1
Makito Katsube <katsubemakito@gmail.com>
2016-06-10 05:55:26 +0000

    change Validation Rule alnum

--- lib/Util/Validation/index.php
+++ lib/Util/Validation/index.php
@@ -153,8 +153,14 @@ class Validation{
 			, 'tel'    => function($val){ return( is_null($val) || $val === "" || preg_match(Regex::TEL,   $val) ); }		// 書式 電話番号 0123-12-1234, 03-12-1234, 090-1234-1234
 
 			, 'num'   => function($val){ return( is_null($val) || $val === "" || is_numeric($val) ); }					// 書式 半角数字(文字列としての数字も真)
-			, 'alpha' => function($val){ return( is_null($val) || $val === "" || preg_match(Regex::ALPHA, $val) ); }		// 書式 半角英字
-			, 'alnum' => function($val){ return( is_null($val) || $val === "" || preg_match(Regex::ALNUM, $val) ); }		// 書式 半角英数字
+			, 'alpha' => function($val){ return( is_null($val) || $val === "" || (is_string($val) && preg_match(Regex::ALPHA, $val)) ); }		// 書式 半角英字
+			, 'alnum' => function($val){	// 書式 半角英数字
+							return(
+									   is_null($val)
+									|| $val === ""
+									|| ( (is_string($val) || is_numeric($val))
+													&& preg_match(Regex::ALNUM, $val) )
+							); }
 
 			, 'require' => function($val){ return( isset($val) && $val !== '' ); }		// 必須項目
 			, 'bytemax' => function($val, $opt){ return( is_null($val) || $val === "" || strlen($val) <= $opt[0] );  }		// 最大バイト長
