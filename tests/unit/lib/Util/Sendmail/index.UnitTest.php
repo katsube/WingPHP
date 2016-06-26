25cb69331e752e9b3c5e26e973cdeeb83f170569
Makito Katsube <katsubemakito@gmail.com>
2016-06-26 13:12:11 +0000

    add Sendmail test code file.

---
+++ tests/unit/lib/Util/Sendmail/index.UnitTest.php
@@ -0,0 +1,99 @@
+<?php
+require_once('define.php');
+require_once('../lib/Util/Validation/index.php');
+
+class UtilSendmailUnitTest extends PHPUnit_Framework_TestCase
+{
+    
+    
+    /**
+     * Test Constructer
+     * 
+     * @covers Sendmail::__construct
+     */
+    public function testConstruct(){
+        $this->markTestIncomplete('Not imprements');
+    }
+
+    /**
+     * Test headers()
+     * 
+     * @covers Sendmail::headers
+     */
+    public function testHeaders(){
+        $this->markTestIncomplete('Not imprements');
+    }
+
+    /**
+     * Test body()
+     * 
+     * @covers Sendmail::body
+     */
+    public function testBody(){
+        $this->markTestIncomplete('Not imprements');
+    }
+
+    /**
+     * Test doit()
+     * 
+     * @covers Sendmail::doit
+     */
+    public function testDoit(){
+        $this->markTestIncomplete('Not imprements');
+    }
+
+    /**
+     * Test setLanguage
+     * 
+     * @covers Sendmail::setLanguage
+     */
+    public function testSetLanguage(){
+        $this->markTestIncomplete('Not imprements');
+    }
+
+    /**
+     * Test setEncording()
+     * 
+     * @covers Sendmail::setEncording
+     */
+    public function testSetEncording(){
+        $this->markTestIncomplete('Not imprements');
+    }
+
+    /**
+     * Test setLogging
+     * 
+     * @covers Sendmail::setLogging
+     */
+    public function testSetLogging(){
+        $this->markTestIncomplete('Not imprements');
+    }
+
+    /**
+     * Test _checkParam()
+     * 
+     * @covers Sendmail::_checkParam
+     */
+    public function testCheckParam(){
+        $this->markTestIncomplete('Not imprements');
+    }
+
+    /**
+     * Test _makeHeader()
+     * 
+     * @covers Sendmail::_makeHeader
+     */
+    public function testMakeHeader(){
+        $this->markTestIncomplete('Not imprements');
+    }
+
+    /**
+     * Test _makeBody()
+     * 
+     * @covers Sendmail::_makeBody
+     */
+    public function testMakeBody(){
+        $this->markTestIncomplete('Not imprements');
+    }
+
+}
