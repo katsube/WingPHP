2e458318bc68b7483d2c34319c7bb4f0bd735454
Makito Katsube <katsubemakito@gmail.com>
2016-06-29 09:42:30 +0000

    add test code that constructor

--- tests/unit/lib/Sendmail/index.UnitTest.php
+++ tests/unit/lib/Sendmail/index.UnitTest.php
@@ -8,10 +8,36 @@ class SendmailUnitTest extends PHPUnit_Framework_TestCase
      * Test Constructer
      * 
      * @covers Sendmail::__construct
+     * @dataProvider ConstructProvider
      */
-    public function testConstruct(){
-        $this->markTestIncomplete('Not imprements');
+    public function testConstruct($setting){
+        global $Conf;
+        $Conf['Sendmail'] = $setting;
+    
+        $mail = new Sendmail();
+        $this->assertEquals($setting['language'],  $mail->getLanguage());
+        $this->assertEquals($setting['encode'],    $mail->getEncording());
+        $this->assertEquals($setting['header'],    $mail->getHeaders());
+        $this->assertEquals($setting['log']['on'], $mail->isLogging());
     }
+
+    public function ConstructProvider(){
+        return(array(
+            array([
+                  'language' => 'Japanese'
+                , 'encode'   => 'Shift_JIS'
+                , 'header'   => ['From'=>'foo@example.com', 'Content-type'=>'text/plain', 'MIME-Version'=>'1.0', 'X-Mailer'=>'I am Tester']
+                , 'log'      => ['on'=>false]
+            ])
+            , array([
+                  'language' => 'English'
+                , 'encode'   => 'UTF-8'
+                , 'header'   => ['From'=>'bar@example.com', 'Content-type'=>'text/plain', 'MIME-Version'=>'1.0', 'X-Mailer'=>'I am Tester']
+                , 'log'      => ['on'=>true]
+            ])
+        ));
+    }
+
 
     /**
      * Test headers()
@@ -19,6 +45,15 @@ class SendmailUnitTest extends PHPUnit_Framework_TestCase
      * @covers Sendmail::headers
      */
     public function testHeaders(){
+        $this->markTestIncomplete('Not imprements');
+    }
+
+    /**
+     * Test get headers()
+     * 
+     * @covers Sendmail::getHeaders
+     */
+    public function testGetHeaders(){
         $this->markTestIncomplete('Not imprements');
     }
 
@@ -50,6 +85,15 @@ class SendmailUnitTest extends PHPUnit_Framework_TestCase
     }
 
     /**
+     * Test getLanguage
+     * 
+     * @covers Sendmail::getLanguage
+     */
+    public function testGetLanguage(){
+        $this->markTestIncomplete('Not imprements');
+    }
+
+    /**
      * Test setEncording()
      * 
      * @covers Sendmail::setEncording
@@ -59,11 +103,29 @@ class SendmailUnitTest extends PHPUnit_Framework_TestCase
     }
 
     /**
+     * Test getEncording()
+     * 
+     * @covers Sendmail::getEncording
+     */
+    public function testGetEncording(){
+        $this->markTestIncomplete('Not imprements');
+    }
+
+    /**
      * Test setLogging
      * 
      * @covers Sendmail::setLogging
      */
     public function testSetLogging(){
+        $this->markTestIncomplete('Not imprements');
+    }
+
+    /**
+     * Test isLogging
+     * 
+     * @covers Sendmail::isLogging
+     */
+    public function testIsLogging(){
         $this->markTestIncomplete('Not imprements');
     }
 
