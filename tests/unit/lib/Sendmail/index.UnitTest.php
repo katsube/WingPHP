744f583eb1556943127607be9f9e6a3cf34a4f5b
Makito Katsube <katsubemakito@gmail.com>
2016-06-29 10:47:04 +0000

    write code Sendmail::headers()

--- tests/unit/lib/Sendmail/index.UnitTest.php
+++ tests/unit/lib/Sendmail/index.UnitTest.php
@@ -43,10 +43,85 @@ class SendmailUnitTest extends PHPUnit_Framework_TestCase
      * Test headers()
      * 
      * @covers Sendmail::headers
+     * @dataProvider HeadersProvider
      */
-    public function testHeaders(){
-        $this->markTestIncomplete('Not imprements');
+    public function testHeaders($setting, $expected){
+        if(is_hash($setting)){
+            //conf
+            global $Conf;
+            if( array_key_exists('Sendmail', $Conf) && array_key_exists('header', $Conf['Sendmail']) && is_array($Conf['Sendmail']['header']) ){
+                $setting = array_merge($setting, $Conf['Sendmail']['header']);
+            }
+
+            //default
+            $setting['Content-type'] = 'text/plain';
+            $setting['MIME-Version'] = '1.0';
+            $setting['X-Mailer']     = 'I am Tester';
+        }
+    
+        $mail   = new Sendmail();
+        $ret    = $mail->headers($setting);
+        $header = $mail->getHeaders();
+        
+        $this->assertEquals($expected, $ret);
+        if($ret)
+            $this->assertEquals($setting, $header);
     }
+    
+    public function HeadersProvider(){
+        return(array(
+            //----------------------
+            //単発
+            //----------------------
+              array(['From'        => 'foo@example.com'], true)
+            , array(['Sender'      => 'foo@example.com'], true)
+            , array(['To'          => 'foo@example.com'], true)
+            , array(['To'          => 'foo@example.com, info@example.net'], true)
+            , array(['Cc'          => 'foo@example.com'], true)
+            , array(['Cc'          => 'foo@example.com, foo@example.net'], true)
+            , array(['Bcc'         => 'foo@example.com'], true)
+            , array(['Bcc'         => 'foo@example.com, foo@example.net'], true)
+            , array(['Subject'     => 'foo!baaaaaaaaaaaaaaaa!'], true)
+            , array(['Reply-to'    => 'foo@example.com'], true)
+            , array(['Return-Path' => 'foo@example.com'], true)
+            , array(['Errors-To'   => 'foo@example.com'], true)
+            , array(['Date'        => '2016/07/01 00:00:00'], true)
+            , array(['In-Reply-To' => 'xxxx.xxxx.xxxx@mail.example.com'], true)
+            , array(['References'  => 'xxxx.xxxx.xxxx@mail.example.com'], true)
+            , array(['Message-ID'  => 'xxxx.xxxx.xxxx@mail.example.com'], true)
+            , array(['Precedence'  => 'list'], true)
+            , array(['Precedence'  => 'junk'], true)
+            , array(['Precedence'  => 'bulk'], true)
+
+            //----------------------
+            //複数同時
+            //----------------------
+            , array([
+                      'From' => 'foo@example.com'
+                    , 'To'   => 'info@example.net'
+                ], true)
+            , array([
+                      'From'   => 'foo@example.net'
+                    , 'To'     => 'info@example.com'
+                    , 'Cc'     => 'cc@example.net'
+                    , 'Bcc'    => 'bcc@example.net'
+                    , 'Sender' => 'sender@example.net'
+                ], true)
+
+            //----------------------
+            //エラー
+            //----------------------
+            , array([],                false)
+            , array(['Foobar'=>12345], false)
+            , array('foobar',          false)
+            , array(12345,             false)
+            , array(1.2345,            false)
+            , array(true,              false)
+            , array(false,             false)
+            , array(null,              false)
+        ));
+    }
+
 
     /**
      * Test get headers()
