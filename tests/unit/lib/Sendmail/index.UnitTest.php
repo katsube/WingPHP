01e61742c8355543a57ed831fbe89931c080f12e
Makito Katsube <katsubemakito@gmail.com>
2016-07-10 11:23:15 +0000

    begin writing testGetHeaders()

--- tests/unit/lib/Sendmail/index.UnitTest.php
+++ tests/unit/lib/Sendmail/index.UnitTest.php
@@ -26,13 +26,13 @@ class SendmailUnitTest extends PHPUnit_Framework_TestCase
             array([
                   'language' => 'Japanese'
                 , 'encode'   => 'Shift_JIS'
-                , 'header'   => ['From'=>'foo@example.com', 'Content-type'=>'text/plain', 'MIME-Version'=>'1.0', 'X-Mailer'=>'I am Tester']
+                , 'header'   => ['From'=>'foo@example.com']
                 , 'log'      => ['on'=>false]
             ])
             , array([
                   'language' => 'English'
                 , 'encode'   => 'UTF-8'
-                , 'header'   => ['From'=>'bar@example.com', 'Content-type'=>'text/plain', 'MIME-Version'=>'1.0', 'X-Mailer'=>'I am Tester']
+                , 'header'   => ['From'=>'bar@example.com']
                 , 'log'      => ['on'=>true]
             ])
         ));
@@ -52,11 +52,6 @@ class SendmailUnitTest extends PHPUnit_Framework_TestCase
             if( array_key_exists('Sendmail', $Conf) && array_key_exists('header', $Conf['Sendmail']) && is_array($Conf['Sendmail']['header']) ){
                 $setting = array_merge($setting, $Conf['Sendmail']['header']);
             }
-
-            //default
-            $setting['Content-type'] = 'text/plain';
-            $setting['MIME-Version'] = '1.0';
-            $setting['X-Mailer']     = 'I am Tester';
         }
     
         $mail   = new Sendmail();
@@ -71,7 +66,7 @@ class SendmailUnitTest extends PHPUnit_Framework_TestCase
     public function HeadersProvider(){
         return(array(
             //----------------------
-            //単発
+            // Single
             //----------------------
               array(['From'        => 'foo@example.com'], true)
             , array(['Sender'      => 'foo@example.com'], true)
@@ -94,7 +89,7 @@ class SendmailUnitTest extends PHPUnit_Framework_TestCase
             , array(['Precedence'  => 'bulk'], true)
 
             //----------------------
-            //複数同時
+            // Multiple
             //----------------------
             , array([
                       'From' => 'foo@example.com'
@@ -109,9 +104,10 @@ class SendmailUnitTest extends PHPUnit_Framework_TestCase
                 ], true)
 
             //----------------------
-            //エラー
+            // Error
             //----------------------
             , array([],                false)
+            , array([1,2,3,4,5],       false)
             , array(['Foobar'=>12345], false)
             , array('foobar',          false)
             , array(12345,             false)
@@ -127,10 +123,51 @@ class SendmailUnitTest extends PHPUnit_Framework_TestCase
      * Test get headers()
      * 
      * @covers Sendmail::getHeaders
+     * @dataProvider GetHeadersProvider
      */
-    public function testGetHeaders(){
-        $this->markTestIncomplete('Not imprements');
+    public function testGetHeaders($setting, $name, $expected){
+        $mail   = new Sendmail();
+        $mail->headers($setting);
+
+        $this->assertEquals($expected, $mail->getHeaders($name));
     }
+    
+    public function GetHeadersProvider(){
+        return(array(
+            //----------------------
+            // Single
+            //----------------------
+              array(['From'        => 'foo@example.com'],                   'From',        'foo@example.com')
+            , array(['Sender'      => 'foo@example.com'],                   'Sender',      'foo@example.com')
+            , array(['To'          => 'foo@example.com'],                   'To',          'foo@example.com')
+            , array(['To'          => 'foo@example.com, info@example.net'], 'To',          'foo@example.com, info@example.net')
+            , array(['Cc'          => 'foo@example.com'],                   'Cc',          'foo@example.com')
+            , array(['Cc'          => 'foo@example.com, foo@example.net'],  'Cc',          'foo@example.com, foo@example.net')
+            , array(['Bcc'         => 'foo@example.com'],                   'Bcc',         'foo@example.com')
+            , array(['Bcc'         => 'foo@example.com, foo@example.net'],  'Bcc',         'foo@example.com, foo@example.net')
+            , array(['Subject'     => 'foo!baaaaaaaaaaaaaaaa!'],            'Subject',     'foo!baaaaaaaaaaaaaaaa!')
+            , array(['Reply-to'    => 'foo@example.com'],                   'Reply-to',    'foo@example.com')
+            , array(['Return-Path' => 'foo@example.com'],                   'Return-Path', 'foo@example.com')
+            , array(['Errors-To'   => 'foo@example.com'],                   'Errors-To',   'foo@example.com')
+            , array(['Date'        => '2016/07/01 00:00:00'],               'Date',        '2016/07/01 00:00:00')
+            , array(['In-Reply-To' => 'xxxx.xxxx.xxxx@mail.example.com'],   'In-Reply-To', 'xxxx.xxxx.xxxx@mail.example.com')
+            , array(['References'  => 'xxxx.xxxx.xxxx@mail.example.com'],   'References',  'xxxx.xxxx.xxxx@mail.example.com')
+            , array(['Message-ID'  => 'xxxx.xxxx.xxxx@mail.example.com'],   'Message-ID',  'xxxx.xxxx.xxxx@mail.example.com')
+            , array(['Precedence'  => 'list'],                              'Precedence',  'list')
+            , array(['Precedence'  => 'junk'],                              'Precedence',  'junk')
+            , array(['Precedence'  => 'bulk'],                              'Precedence',  'bulk')
+
+            //----------------------
+            // Multiple
+            //----------------------
+
+            //----------------------
+            // Error
+            //----------------------
+
+        ));
+    }
+
 
     /**
      * Test body()
